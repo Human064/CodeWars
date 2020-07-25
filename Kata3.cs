3aca304dcb17ccaf151d9455af4b52635d688e59
Human064 <58043703+Human064@users.noreply.github.com>
2020-07-25 17:17:49 +0300

    Create Kata3.cs

---
+++ Kata3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            Console.WriteLine(CreatePhoneNumber(arr));
+            Console.ReadKey();
+        }
+        public static string CreatePhoneNumber(int[] numbers)
+        {
+            string res = "(";
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                switch (i)
+                {
+                    case 2:
+                        res += numbers[i] + ") ";
+                        break;
+                    case 5:
+                        res += numbers[i] + "-";
+                        break;
+                    default:
+                        res += numbers[i];
+                        break;
+                }
+            }
+
+            return res;
+        }
+    }
+}

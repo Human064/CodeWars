5a9e88f0d0d8d7688e4448280e74522cdc83a57c
Human064 <58043703+Human064@users.noreply.github.com>
2020-07-25 17:17:59 +0300

    Create Kata5.cs

---
+++ Kata5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] ar = new[] { 'O', 'Q', 'R', 'S' };
+            Console.WriteLine(FindMissingLetter(ar));
+            Console.ReadKey();
+        }
+
+        public static char FindMissingLetter(char[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if ((int)array[i] + 1 != (int)array[i + 1])
+                    return (char)((int)array[i] + 1);
+            }
+            return ' ';
+        }
+    }
+}

ff7c9e9a70e4d48294279758d4beac41746b3116
Human064 <58043703+Human064@users.noreply.github.com>
2020-07-25 09:30:41 +0300

    Функция Sabb
    Learning to code around your full time job is taking over your life. You realise that in order to make significant steps quickly, it would help to go to a coding bootcamp in London.
    
    Problem is, many of them cost a fortune, and those that don't still involve a significant amount of time off work - who will pay your mortgage?!
    
    To offset this risk, you decide that rather than leaving work totally, you will request a sabbatical so that you can go back to work post bootcamp and be paid while you look for your next role.
    
    You need to approach your boss. Her decision will be based on three parameters:
    
    val=your value to the organisation
    happiness=her happiness level at the time of asking and finally
    The numbers of letters from 'sabbatical' that are present in string s.
    
    Note that if s contains three instances of the letter 'l', that still scores three points, even though there is only one in the word sabbatical.
    
    If the sum of the three parameters (as described above) is > 22, return 'Sabbatical! Boom!', else return 'Back to your desk, boy.'.

---
+++ Kata2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sabb("Why are you shouting?", 7, 2));
+            Console.ReadKey();
+        }
+        public static string Sabb(string x, int val, int happ)
+        {
+            int Count = 0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                switch (x[i])
+                {
+                    case 's':
+                        Count++;
+                        break;
+                    case 'S':
+                        goto case 's';
+                    case 'a':
+                        Count++;
+                        break;
+                    case 'A':
+                        goto case 'a';
+                    case 'b':
+                        Count++;
+                        break;
+                    case 'B':
+                        goto case 'b';
+                    case 't':
+                        Count++;
+                        break;
+                    case 'T':
+                        goto case 't';
+                    case 'i':
+                        Count++;
+                        break;
+                    case 'I':
+                        goto case 'i';
+                    case 'c':
+                        Count++;
+                        break;
+                    case 'C':
+                        goto case 'c';
+                    case 'l':
+                        Count++;
+                        break;
+                    case 'L':
+                        goto case 'l';
+                    default:
+                        break;
+                }
+            }
+
+            if ((Count + val + happ) > 22)
+                return "Sabbatical! Boom!";
+            else
+                return "Back to your desk, boy.";
+        }
+    }
+}

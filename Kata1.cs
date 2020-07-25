dbde4b843e970b21fdf81b00fd806837f8ac4b24
Human064 <58043703+Human064@users.noreply.github.com>
2020-07-25 09:30:00 +0300

    Умножение

---
+++ Kata1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 3;
+            Console.WriteLine(multiply(a, b));
+            Console.ReadKey();
+        }
+        public static int multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}

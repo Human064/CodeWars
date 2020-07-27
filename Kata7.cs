a58ea931ef735ea079b37164c7443da8126158c3
Human064 <58043703+Human064@users.noreply.github.com>
2020-07-27 08:45:16 +0300

    Create Kata7.cs
    Поиск панграм без встоенных поисков.

---
+++ Kata7.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPangram("ABCDEFGHIJKLMNOPQRSTUVWXYZ."));
+            Console.ReadKey();
+        }
+        public static bool IsPangram(string str)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            int[] ar = new int[alphabet.Length];
+            for (int i = 0; i < ar.Length; i++)
+                ar[i] = 0;
+
+            int res = 0;
+
+            str = str.ToUpper();
+
+            for (int i=0;i<str.Length;i++)
+                for (int j = 0; j < alphabet.Length; j++)
+                    if (str[i] == alphabet[j])
+                        ar[j]++;
+
+            for (int i = 0; i < ar.Length; i++)
+                if (ar[i] == 1)
+                    res++;
+
+            return res==26;
+        }
+    }
+}

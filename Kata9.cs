d9f27753658df8e6e75c54869a55922c49cc2651
Human064 <58043703+Human064@users.noreply.github.com>
2020-07-30 08:37:17 +0300

    Create Kata9.cs
    Смещение всех нулей из массива вправо с сохранением всех значений

---
+++ Kata9.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }));
+            Console.ReadKey();
+        }
+        public static int[] MoveZeroes(int[] arr)
+        {
+            int[] Buff = arr;
+            int j = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] != 0)
+                {
+                    Buff[j] = arr[i];
+                    j++;
+                }
+
+            while (j != Buff.Length)
+            {
+                Buff[j] = 0;
+                j++;
+            }
+
+            return Buff;
+
+            // TODO: Program me
+        }
+    }
+}

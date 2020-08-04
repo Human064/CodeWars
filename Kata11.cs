21be617cfd0aab064c13bd8a2593621d262d0168
Human064 <58043703+Human064@users.noreply.github.com>
2020-08-04 07:25:16 +0300

    Create Kata11.cs
    Функция, определяющая возможность деления n на x и y

---
+++ Kata11.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(isDivisible(8, 3, 4));
+            Console.ReadKey();
+
+        }
+        public static bool isDivisible(long n, long x, long y)
+        {
+            return (n % x == 0) && (n % y == 0);
+        }
+    }
+}

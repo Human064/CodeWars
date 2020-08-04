a9c6c5b5a3ebb9600da9e82680f4b6a54cfee036
Human064 <58043703+Human064@users.noreply.github.com>
2020-08-04 08:04:23 +0300

    Create Kate13.cs
    Расчёт потребляемой воды при потреблении 0.5 литров в час и округлении до целого в меньшую сторону.

---
+++ Kate13.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Litres(3));
+            Console.ReadKey();
+
+        }
+        public static int Litres(double time)
+        {
+            return Convert.ToInt32(Math.Floor(time * 0.5));
+        }
+    }
+}

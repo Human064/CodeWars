17fdf05c54f4faabe9b6e8e83c2e88e225d83a5e
Human064 <58043703+Human064@users.noreply.github.com>
2020-08-04 07:30:11 +0300

    Create Kata12.cs
    Удаление пробелов из строки

---
+++ Kata12.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NoSpace("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd"));
+            Console.ReadKey();
+
+        }
+        public static string NoSpace(string input)
+        {
+            string buf = "";
+            for (int i = 0; i < input.Length; i++)
+                if (input[i] != ' ')
+                    buf += input[i];
+
+            return buf;
+        }
+    }
+}
